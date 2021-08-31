41e8488ba51303a94171e17fc8567bfdefd3ccb1
jangilyong <kaitaro54@gmail.com>
2021-08-31 18:02:28 +0900

    테스트 컴잇

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Linkoo App'),
+      home: MyHomePage(title: 'Linkoo App2'),
     );
   }
 }
